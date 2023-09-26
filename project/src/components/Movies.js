3eea14ed21841300c66a24a9320856404c157ab1
Adrian <adrianqueizan@gmail.com>
2023-09-26 14:30:42 -0400

    work updated

--- project/src/components/Movies.js
+++ project/src/components/Movies.js
@@ -6,6 +6,7 @@ function Movies({filteredArray,handleDelete}){
     const mappedArray= filteredArray.map(movie=>(<Movie key={movie.id} id={movie.id} name={movie.Title} year={movie.Year} runTime={movie.Runtime} rated={movie.Rated} released={movie.Released}  genre={movie.Genre} director={movie.Director} actors={movie.Actors} plot={movie.Plot} awards={movie.Awards} poster={movie.Poster} handleDelete={handleDelete}/>))
     
     return(
+        
         <div className="card-container">
             {mappedArray}
         </div>)
